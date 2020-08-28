8139313390dc4ef0916758f79ef783efeb0c4d7e
muktaahammed <mukta192426@gmail.com>
2020-08-28 20:41:23 +0800

    first commit

---
+++ Readme.txt
@@ -0,0 +1,14 @@
+
+First >> Create a Database named "( dbplayers )"
+
+Then >> Import the sql into the database.
+
+Then >> Paste player folder into your www directory
+
+Then >> Open localhost on your browser
+
+Well Done !!! You did it. Now customize as your own.
+
+Note: If you use online server just modify the connect-db
+
+
---
+++ players.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 28, 2020 at 07:23 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbplayer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(32) NOT NULL,
+  `lastname` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `players`
+--
+
+INSERT INTO `players` (`id`, `firstname`, `lastname`) VALUES
+(1, 'Mukta', 'Ahammed'),
+(2, 'Salim', 'Hosen'),
+(3, 'Ariful', 'Islam'),
+(4, 'Jahangiir', 'Alam');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
